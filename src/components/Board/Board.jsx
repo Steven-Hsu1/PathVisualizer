7d179e31fd6fcd74341f953dd83687b3b5966ab5
Steven Hsu <sthsu@ucsd.edu>
2023-09-14 10:51:02 -0700

    sync

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,6 +1,53 @@
 import React from "react";
+import { useState } from "react";
 import './Board.scss';
 
+
+const generateBoard = (row, col) => {
+    const newBoard = [];
+    for (let i = 0; i < row; i++) {
+        newBoard.push([...Array(col)]);
+    }
+    return newBoard;
+}
+
 export default function Board() {
-    return (<div id="board">Hi</div>);
+    let vpWidth = window.innerWidth;
+    window.onresize = () => {
+        vpWidth = window.innerWidth;
+    }
+    // const vpHeight = window.innerHeight;
+    const numCols = Math.floor(vpWidth / 50);
+    const [board, setBoard] = useState(generateBoard(10, numCols));
+    return (
+        <div id="board">
+            {
+                board.map((row, r) => {
+                    return (
+                        <div 
+                        key={r}
+                        style={{
+                            display: 'flex'
+                        }}>
+                            {row.map((cell, c) => {
+                                return (
+                                    <div 
+                                    key={c} 
+                                    style={{
+                                        border: 'solid black 1px',
+                                        height: '50px',
+                                        width: '50px',
+                                        display: 'flex',
+                                        justifyContent: 'center'
+                                    }}>
+                                        {cell}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
 }
